5f79807372c04f6b51a24d7b0dcb174c4431ebaa
lgomez@cvc.uab.es <lgomez@etse-75-51.uab.es>
2019-03-13 10:29:00 +0100

    do not random_dist test images

--- dataloader/dataloader_builder.py
+++ dataloader/dataloader_builder.py
@@ -23,10 +23,14 @@ class Dataloader_Builder(object):
         if self.cf.problem_type == 'detection':
             self.img_preprocessing = standard_transforms.Compose([Random_distort(self.cf), preproces_input(self.cf),
                                                                   standard_transforms.ToTensor()])
+            self.test_img_preprocessing = standard_transforms.Compose([preproces_input(self.cf),
+                                                                  standard_transforms.ToTensor()])
             self.train_transformation = ComposeObjDet([CropObjDet(self.cf), RandomHorizontalFlipObjDet(self.cf)])
             self.resize = ComposeResize([Resize(self.cf)])
         else:
             self.img_preprocessing = standard_transforms.Compose([Random_distort(self.cf), preproces_input(self.cf),
+                                                                  ToTensor()])
+            self.test_img_preprocessing = standard_transforms.Compose([preproces_input(self.cf),
                                                                   ToTensor()])
             self.train_transformation = ComposeSemSeg([CropSegSem(self.cf),
                                                        RandomHorizontalFlipSegSem(self.cf)])
@@ -73,5 +77,5 @@ class Dataloader_Builder(object):
     def build_predict(self):
         self.predict_set = fromFileDatasetToPredict(self.cf, self.cf.test_images_txt,
                                                self.cf.test_samples, self.cf.resize_image_test,
-                                               preprocess=self.img_preprocessing)
+                                               preprocess=self.test_img_preprocessing)
         self.predict_loader = DataLoader(self.predict_set, batch_size=1, num_workers=8)
